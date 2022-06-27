57945325cba35c59e6bd587ad53619a1c80f2556
tatianadavila91 <tatiana.davila@devitech.com.co>
2022-06-27 15:15:20 -0500

    create previousrulinglayout with styles module and exports index files

---
+++ src/renderers/previousRulingLayout/previousRulingLayout.tsx
@@ -0,0 +1,10 @@
+export const PreviousRulingLayout: React.FunctionComponent = () => {
+    return (
+        <section className="prl__container">
+            <div className="prl__header">
+                <p>Previous Rulings</p>
+            </div>
+            <div className="prl__box-overflow"></div>
+        </section>
+    )
+}
