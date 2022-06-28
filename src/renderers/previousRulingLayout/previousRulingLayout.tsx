8678a7f7d399c8ffa5f49af31180b09f46a2920e
tatadavila <tatdev91@gmail.com>
2022-06-28 00:17:11 -0500

    change name to tomcoment and add header title

--- src/renderers/previousRulingLayout/previousRulingLayout.tsx
+++ src/renderers/previousRulingLayout/previousRulingLayout.tsx
@@ -2,7 +2,7 @@ export const PreviousRulingLayout: React.FunctionComponent = () => {
     return (
         <section className="prl__container">
             <div className="prl__header">
-                <p>Previous Rulings</p>
+                <p className="prl__header-title">Previous Rulings</p>
             </div>
             <div className="prl__box-overflow"></div>
         </section>
