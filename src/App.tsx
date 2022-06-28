5f4e424a3afac84796bb3d8a1c077bee52d0ea4c
tatadavila <tatdev91@gmail.com>
2022-06-28 01:00:15 -0500

    import app styles file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { PreviousRulingLayout } from './renderers'
+// @styles
+import './styles/scss/app.sass'
+
+import { PreviousRulingLayout } from './renderers/previousRulingLayout'
 
 const App = () => {
     return <PreviousRulingLayout />
