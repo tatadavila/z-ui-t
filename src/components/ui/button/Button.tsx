4e82e1ecc8b70f998a0bc1607ce29be179e1756d
tatadavila <tatdev91@gmail.com>
2022-07-01 07:49:01 -0500

    add disabled boolean variable to the buttons properties and interface

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,7 +1,8 @@
 interface ButtonI {
     children: React.ReactNode
+    disabled: boolean
 }
 
-export const Button: React.FC<ButtonI> = ({ children }) => {
+export const Button: React.FC<ButtonI> = ({ children, disabled=true }) => {
     return <button className="btn__container">{children}</button>
 }
