7de9f63961846cade24ac143370872edc8684eec
tatadavila <tatdev91@gmail.com>
2022-07-03 00:20:05 -0500

    modify button component implement clickhandler for onclick property

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,8 +1,17 @@
-interface ButtonI {
-    children: React.ReactNode
-    disabled: boolean
-}
+// @interface
+import { ButtonI } from './button.interface'
 
-export const Button: React.FC<ButtonI> = ({ children, disabled=true }) => {
-    return <button className="btn__container">{children}</button>
+export const Button: React.FC<ButtonI> = ({
+    children,
+    disabled = true,
+    onClickHandler,
+}) => {
+    return (
+        <button
+            className={`btn__container ${disabled ? 'btn__disabled' : ''}`}
+            onClick={() => onClickHandler('voteNow')}
+        >
+            {children}
+        </button>
+    )
 }
