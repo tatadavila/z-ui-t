b4097c3e1ddc37e374bd01c73d3bdcf65969e1fd
tatadavila <tatdev91@gmail.com>
2022-07-03 00:27:36 -0500

    implement deletespaces function for mapped key

--- src/components/viewType/ViewType.tsx
+++ src/components/viewType/ViewType.tsx
@@ -4,14 +4,20 @@ import { data as mockedData } from '~/assets/data.json'
 // @interfaces
 import { ViewTypeI } from '~/interfaces'
 
+// @utils
+import { deleteSpaces } from '~/utils'
+
 export const ViewType: any = (
     PassedComponent: React.ComponentType<ViewTypeI>,
 ) => {
-    const WrappedComponent = () => {
+    const WrappedComponent: React.FC = () => {
         return (
-            <div className='vt__scrolling-container'>
+            <div className="vt__scrolling-container">
                 {mockedData.map(element => (
-                    <PassedComponent data={element} key={element.name} />
+                    <PassedComponent
+                        data={element}
+                        key={deleteSpaces(element.name)}
+                    />
                 ))}
             </div>
         )
