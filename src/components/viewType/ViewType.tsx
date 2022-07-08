c0bdd1d9f624944e53f970e34a6d863991a1b7d6
tatadavila <tatdev91@gmail.com>
2022-07-08 01:08:16 -0500

    Modify viewType component

--- src/components/viewType/ViewType.tsx
+++ src/components/viewType/ViewType.tsx
@@ -1,26 +1,37 @@
 // @assets
-import { data as mockedData } from '~/assets/data.json'
+import { data } from '~/assets/data.json'
+
+// @components
+import { Box } from '~/components'
+
+// @localStore
+import { setItemLocalStorage } from '~/localStore'
 
 // @interfaces
-import { ViewTypeI } from '~/interfaces'
+import { DataI } from '~/interfaces'
 
 // @utils
 import { deleteSpaces } from '~/utils'
 
-export const ViewType: any = (
-    PassedComponent: React.ComponentType<ViewTypeI>,
-) => {
-    const WrappedComponent: React.FC = () => {
-        return (
-            <div className="vt__scrolling-container">
-                {mockedData.map(element => (
-                    <PassedComponent
-                        data={element}
-                        key={deleteSpaces(element.name)}
-                    />
-                ))}
-            </div>
-        )
-    }
-    return WrappedComponent
+// @vendors
+import { FC, useState, useEffect } from 'react'
+
+export const ViewType: FC = () => {
+    const [celebrityData, setCelebrityData] = useState<DataI[]>(data)
+    useEffect(() => {
+        const items = { ...localStorage }
+        if (Object.keys(items).length === 0) {
+            setItemLocalStorage('celebrities', data)
+        } else {
+            console.log('Entered else')
+        }
+    }, [])
+
+    return (
+        <div className="vt__scrolling-container">
+            {celebrityData.map((element, key) => (
+                <Box data={element} key={key} setData={setCelebrityData} />
+            ))}
+        </div>
+    )
 }
