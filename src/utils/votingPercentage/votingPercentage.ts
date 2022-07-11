46ed9833ea132e13fb05a0413e30d0ee04b5a626
tatadavila <tatdev91@gmail.com>
2022-07-11 05:44:21 -0500

    remove fuction return type and unused import

--- src/utils/votingPercentage/votingPercentage.ts
+++ src/utils/votingPercentage/votingPercentage.ts
@@ -1,9 +1,5 @@
-import { VotingPercentageI } from '~/interfaces/votingPercentage.interface'
 
-export const votingPercentage = (
-    negative: number,
-    positive: number,
-): VotingPercentageI => {
+export const votingPercentage = (negative: number, positive: number) => {
     return {
         negative: Math.round((negative / (negative + positive)) * 100),
         positive: Math.round((positive / (negative + positive)) * 100),
