d6062eb98ed499e14128688a8dd68ad7807ca467
tatadavila <tatdev91@gmail.com>
2022-07-11 05:38:41 -0500

    implement select component

--- src/renderers/previousRulingLayout/previousRulingLayout.tsx
+++ src/renderers/previousRulingLayout/previousRulingLayout.tsx
@@ -1,11 +1,13 @@
 // @component
 import { ViewType } from '~/components'
+import { Select } from '~/components/ui/select'
 
 export const PreviousRulingLayout: React.FC = () => {
     return (
         <section className="prl__container">
             <div className="prl__header">
                 <p className="prl__header-title">Previous Rulings</p>
+                <Select type="Grid" />
             </div>
             <section className="prl__boxes">
                 <ViewType />
